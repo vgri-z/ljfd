75264fbbff3e9b19cb8c08920bed5cdf73e1c141
vgri <122983719@qq.com>
2022-04-22 14:43:20 +0800

    角色删除，内容管理列表页

--- src/views/main/system-manage/role/role.vue
+++ src/views/main/system-manage/role/role.vue
@@ -27,6 +27,11 @@
         <el-table-column label="操作">
           <template v-slot="scope">
             <el-button type="text" size="small" @click="edit(scope.row)">编辑</el-button>
+            <el-popconfirm title="是否确认删除该角色？" @confirm="remove(scope.row.id)">
+              <template #reference>
+                <el-button type="text" style="color: #f56c6c" size="small">删除</el-button>
+              </template>
+            </el-popconfirm>
           </template>
         </el-table-column>
       </el-table>
@@ -50,8 +55,9 @@
 </template>
 
 <script>
-import { getRoleList } from '../../../../service/main/role/role'
+import { getRoleList, deleteRole } from '../../../../service/main/role/role'
 import Edit from './edit.vue'
+import { ElMessage } from 'element-plus'
 export default {
   components: {
     Edit
@@ -78,6 +84,16 @@ export default {
     reset() {
       this.searchOption.Filter = ''
       this.getRoleList()
+    },
+    async remove(id) {
+      const res = await deleteRole(id)
+      if (res.code === 0) {
+        ElMessage({
+          message: '操作成功',
+          type: 'success'
+        })
+        this.getRoleList()
+      }
     },
     async getRoleList() {
       const res = await getRoleList(this.searchOption)
